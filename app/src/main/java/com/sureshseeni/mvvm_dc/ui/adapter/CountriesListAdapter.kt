f73d8d9922dbd2dcd8d572f4cd9c0e60d46f7eb9
Suresh <suresh.s@appoets.com>
2020-12-01 19:54:25 +0530

    Adatper binding also done

--- app/src/main/java/com/sureshseeni/mvvm_dc/ui/adapter/CountriesListAdapter.kt
+++ app/src/main/java/com/sureshseeni/mvvm_dc/ui/adapter/CountriesListAdapter.kt
@@ -1,28 +1,36 @@
 package com.sureshseeni.mvvm_dc.ui.adapter
 
-import android.app.Activity
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sureshseeni.mvvm_dc.R
+import com.sureshseeni.mvvm_dc.databinding.RowCountryListingItemBinding
 import com.sureshseeni.mvvm_dc.network.model.RestCountries
-import kotlinx.android.synthetic.main.row_country_listing_item.view.*
 
-
-class CountriesListAdapter(val ctx: Context, private val list: List<RestCountries.RestCountriesItem>) :
+class CountriesListAdapter(
+    val ctx: Context,
+    private val list: List<RestCountries.RestCountriesItem>
+) :
     RecyclerView.Adapter<CountriesListAdapter.WelcomeActivityActivityViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WelcomeActivityActivityViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_country_listing_item, null)
-        return WelcomeActivityActivityViewHolder(view)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): WelcomeActivityActivityViewHolder {
+        return WelcomeActivityActivityViewHolder(
+            DataBindingUtil.inflate<RowCountryListingItemBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.row_country_listing_item,
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: WelcomeActivityActivityViewHolder, position: Int) {
-        holder.itemView.tv_country.text = list[position].name
-
-
+        holder.itemBinding.tvCountry.text = list[position].name
 
     }
 
@@ -30,6 +38,10 @@ class CountriesListAdapter(val ctx: Context, private val list: List<RestCountrie
         return list.size
     }
 
-    inner class WelcomeActivityActivityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class WelcomeActivityActivityViewHolder(itemView: RowCountryListingItemBinding) :
+        RecyclerView.ViewHolder(itemView.root) {
+        val itemBinding = itemView
+    }
+
 
 }
